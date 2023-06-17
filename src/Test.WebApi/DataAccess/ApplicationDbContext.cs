1176c312dfe8b03ad5059cd1cd9c2e38e5e9a8b9
Long Le <lv.long.tn@gmail.com>
2023-06-17 22:45:16 +0700

    Init DB

---
+++ src/Test.WebApi/DataAccess/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Test.WebApi.DataAccess
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<AccessAttribute> AccessAttributes { get; set; }
+
+        public DbSet<AccessAttributeTable> AccessAttributeTables { get; set; }
+
+        public DbSet<DataPermission> DataPermissions { get; set; } 
+    }
+}
---
+++ src/Test.WebApi/Migrations/20230617154245_InitDatabase.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Test.WebApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitDatabase : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "AccessAttributes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Code = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AccessAttributes", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "AccessAttributeTables",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    AccessAttributeId = table.Column<int>(type: "int", nullable: false),
+                    TableName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Alias = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    IdColumn = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    NameColumn = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ParentColumn = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    HiearchyLevel = table.Column<int>(type: "int", nullable: false),
+                    LocalPermissionTableName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    LocalPermissionIdColumn = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    LocalPermissionLookupColumn = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AccessAttributeTables", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "DataPermissions",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    GrantType = table.Column<int>(type: "int", nullable: false),
+                    SubjectId = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    AccessAttributeTableId = table.Column<int>(type: "int", nullable: false),
+                    AccessLevel = table.Column<int>(type: "int", nullable: false),
+                    GrantedDataValue = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DataPermissions", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "AccessAttributes");
+
+            migrationBuilder.DropTable(
+                name: "AccessAttributeTables");
+
+            migrationBuilder.DropTable(
+                name: "DataPermissions");
+        }
+    }
+}
