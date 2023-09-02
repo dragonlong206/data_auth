a53d4e2dfd5378501a038a183d76c0040e9ddac1
Long Le <lv.long.tn@gmail.com>
2023-09-02 21:53:10 +0700

    Add nuget.config for linking between nuget and github

---
+++ src/DataAuth/Enums/FunctionCode.cs
@@ -0,0 +1,10 @@
+﻿namespace DataAuth.Enums
+{
+    public static partial class FunctionCode
+    {
+        public const string Create = "Create";
+        public const string Read = "Read";
+        public const string Delete = "Delete";
+        public const string Update = "Update";
+    }
+}
